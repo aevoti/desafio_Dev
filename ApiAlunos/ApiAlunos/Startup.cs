b0c5a1acabd31e8c8f13f9c2e072b90c64985db7
Lucas <lucaspanetto@ucl.br>
2020-07-24 19:16:46 -0300

    Implementando Swagger, redirecionando página inicial, removendo controller desnecessário

--- ApiAlunos/ApiAlunos/Startup.cs
+++ ApiAlunos/ApiAlunos/Startup.cs
@@ -12,6 +12,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
+using Microsoft.OpenApi.Models;
 
 namespace ApiAlunos
 {
@@ -41,6 +43,19 @@ namespace ApiAlunos
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Latest);
             services.AddMvc(option => option.EnableEndpointRouting = false);
+
+            //Implementando swagger
+            services.AddSwaggerGen(c =>
+            {
+
+                c.SwaggerDoc("v1",
+                    new OpenApiInfo
+                    {
+                        Title = "Indicadores Econômicos",
+                        Version = "v1",
+                        Description = "Exemplo de API REST criada com o ASP.NET Core 2.2 para consulta a indicadores econômicos"
+                    });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -57,6 +72,12 @@ namespace ApiAlunos
             }
 
             app.UseCors("EnableCORS");
+
+            // Ativando middlewares para uso do Swagger
+            app.UseSwagger();
+            app.UseSwaggerUI(c => {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Indicadores Econômicos V1");
+            });
 
             app.UseHttpsRedirection();
             app.UseMvc();
