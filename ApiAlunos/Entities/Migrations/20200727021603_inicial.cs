6f29cc23829600ed49348bf6782c15f0b3a199f7
Douglas Sousa <douglas.sousa@phidelis.com.br>
2020-07-26 23:34:50 -0300

    Adicionado projeto para as entidades; configuração de paginação e parametros de busca; e nova configuração de migração;

---
+++ ApiAlunos/Entities/Migrations/20200727021603_inicial.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Entities.Migrations
+{
+    public partial class inicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Alunos",
+                columns: table => new
+                {
+                    AlunoId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Alunos", x => x.AlunoId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Alunos");
+        }
+    }
+}
---
+++ ApiAlunos/Entities/RepositoryContext.cs
@@ -0,0 +1,36 @@
+﻿using Entities.Configuration;
+using Entities.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace Entities.Context
+{
+    public class RepositoryContext : DbContext
+    {
+        public RepositoryContext(DbContextOptions<RepositoryContext> options) : base(options)
+        {
+        }
+        public DbSet<Aluno> Alunos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new AlunoConfiguration());
+        }
+    }
+
+    public class DesignTimeContextFactory: IDesignTimeDbContextFactory<RepositoryContext>
+    {
+        public RepositoryContext CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile(Directory.GetCurrentDirectory() + "/../ApiAlunos/appsettings.json").Build();
+            var builder = new DbContextOptionsBuilder<RepositoryContext>();
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+            builder.UseSqlServer(connectionString);
+
+            return new RepositoryContext(builder.Options);
+
+        }
+    }
+}
