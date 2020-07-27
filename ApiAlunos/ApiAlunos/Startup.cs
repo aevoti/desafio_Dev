25de6e24a0a1b61654277590e8d295224c7dd045
Douglas Sousa <douglas.sousa@phidelis.com.br>
2020-07-26 23:32:28 -0300

    Adicionado extensão de serviço para adicionar repositorio ao escopo

--- ApiAlunos/ApiAlunos/Startup.cs
+++ ApiAlunos/ApiAlunos/Startup.cs
@@ -1,17 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using ApiAlunos.Context;
+﻿using ApiAlunos.Extensions;
+using Entities.Context;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 
 namespace ApiAlunos
 {
@@ -35,12 +29,15 @@ namespace ApiAlunos
                 });
             });
 
-            services.AddDbContext<AppDbContext>(options =>
+            services.AddDbContext<RepositoryContext>(options =>
                     options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"))
                 );
 
+            services.ConfigureRepositoryWrapper();
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Latest);
             services.AddMvc(option => option.EnableEndpointRouting = false);
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
