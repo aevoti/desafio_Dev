425825906b1db7530c1ee8cf2de3a8dac5bdda18
dodevs <douglass.sousa@outlook.com.br>
2020-07-28 00:13:54 -0300

    Criado modulo para angular material

--- FrontAlunos/src/app/app.module.ts
+++ FrontAlunos/src/app/app.module.ts
@@ -4,13 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { MaterialModule } from './shared/material.module';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
