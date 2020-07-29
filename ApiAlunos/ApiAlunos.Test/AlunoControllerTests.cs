23e633ff4c4b341140287a828d12106cad6d46a5
Douglas Sousa <douglas.sousa@phidelis.com.br>
2020-07-28 22:03:09 -0300

    Implementação inicial dos testes para o controlador

---
+++ ApiAlunos/ApiAlunos.Test/AlunoControllerTests.cs
@@ -0,0 +1,36 @@
+﻿using ApiAlunos.Controllers;
+using Contracts;
+using Entities.Context;
+using Microsoft.EntityFrameworkCore;
+using Repository;
+
+namespace ApiAlunos.Test
+{
+    public class AlunoControllerTests
+    {
+        private readonly AlunosController _controller;
+
+        public AlunoControllerTests()
+        {
+            _controller = new AlunosController(GetAlunoInMemoryRepository());
+        }
+
+        #region POST
+        #endregion POST
+
+        private IRepositoryWrapper GetAlunoInMemoryRepository()
+        {
+            DbContextOptions<RepositoryContext> options;
+            var builder = new DbContextOptionsBuilder<RepositoryContext>();
+            builder.UseInMemoryDatabase(databaseName: "ConsultasDBTest");
+            options = builder.Options;
+
+            RepositoryContext repositoryContext = new RepositoryContext(options);
+
+            repositoryContext.Database.EnsureDeleted();
+            repositoryContext.Database.EnsureCreated();
+
+            return new RepositoryWrapper(repositoryContext);
+        }
+    }
+}
