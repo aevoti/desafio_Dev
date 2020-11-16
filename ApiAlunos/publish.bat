60e039c4c5ebb8c479b065bc465762671bcd5965
yanpitangui <yanpitangui@gmail.com>
2020-11-16 20:40:03 -0300

    publicação de aplicações no heroku

---
+++ ApiAlunos/publish.bat
@@ -0,0 +1,2 @@
+heroku container:push web -a desafio-aevo-api
+heroku container:release web -a desafio-aevo-api
--- FrontAlunos/src/environments/environment.prod.ts
+++ FrontAlunos/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https:localhost:5001/api/'
+  apiUrl: 'https://desafio-aevo-api.herokuapp.com/api/',
 };
